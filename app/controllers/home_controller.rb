aef8d172fe877dd27e668e4a16c40e7928b56164
Asim Adnan Eijaz <asimadnaneijaz@hotmail.com>
2019-05-03 02:18:26 +1000

    work done

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,20 @@
 class HomeController < ApplicationController
   
   def index
-    
+    config = {
+        consumer_key:    "Mrob8koLQ3WYPE6RVADwOnnz4",
+        consumer_secret: "wWnBOZr6138UnBwPVaB9iBJLMSV8nzCyvxk8CB8Wl2R9vOMRmP",
+    }
+
+    client = Twitter::REST::Client.new(config)
+
+    @user_tweets = client.user_timeline('ResonateAU')
+
+    @tweets_html = {}
+    @user_tweets.each do |a|
+      @tweets_html[a.id] = client.oembed(a).html
+    end
+
   end
-  
+
 end
